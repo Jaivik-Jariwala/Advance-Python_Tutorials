68ed67584468c31865f0d38e45d1fd20472f5491
Jaivik Jariwala <112539487+Jaivik-Jariwala@users.noreply.github.com>
2023-10-09 09:30:43 +0530

    added : selenium

---
+++ week_9/day_1/selenium.py
@@ -0,0 +1,8 @@
+# opening a webpage and location elements 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Firefox()
+driver.get("https://www.python.org/")
+
+#locate an element by id
+
