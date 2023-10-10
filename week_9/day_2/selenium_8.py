ebfaf35b5c0c6129d22c302494631495c963d9e7
Jaivik Jariwala <112539487+Jaivik-Jariwala@users.noreply.github.com>
2023-10-10 09:37:47 +0530

    added sele

---
+++ week_9/day_2/selenium_8.py
@@ -0,0 +1,19 @@
+#open a webpage scroll down to load dynamic contetn and captie the page source acfte the scrolling
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+driver=webdriver.Firefox()
+driver.get("")
+
+#scroll down tolad dynamic content 
+driver.execute_script("window.scollTo(0, doscuments.body.scrollHeight);")
+
+#retrerive the page source HTML of the webpage ftet the scrollign action 
+page_source=driver.page_source
+
+#parse the paf source uing beautifulsoup
+soup = BeautifulSoup(page_source,'html.parser')
+
+#print the title tag
+print("title:", soup.title.text)
+
